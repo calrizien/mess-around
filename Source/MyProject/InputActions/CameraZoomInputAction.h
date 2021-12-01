3a1cbaf4b5b40c8273d6e7460835bfdf2624aeb7
Brandon Winston <brandonjwinston@gmail.com>
2021-12-01 15:20:55 -0700

    CLeaning up

---
+++ Source/MyProject/InputActions/CameraZoomInputAction.h
@@ -0,0 +1,17 @@
+// Copyright Brandon Winston 2021
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "InputAction.h"
+#include "CameraZoomInputAction.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class MYPROJECT_API UCameraZoomInputAction : public UInputAction
+{
+	GENERATED_BODY()
+	
+};
